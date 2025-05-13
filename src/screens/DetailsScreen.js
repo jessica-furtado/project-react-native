9d873c14b49a73e60a6401cad74ccee9dbdbc684
Jéssica Furtado <jessicafurtado.adm@gmail.com>
2025-05-13 20:53:38 -0300

    Atualizações aula03

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const DetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>📄 Detalhes do Curso</Text>
+      <text style={styles.itemTitle}>Curso de AWS</text>
+      <text style={styles.itemDescription}>...</text>
+      <text style={styles.itemId}>ID:...</text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: '#f5f5f5' },
+    title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20 },
+    itemTitle: { fontSize: 20, fontWeight: 'bold', marginBottom: 10 },
+    itemDescription: { fontSize: 16, color: '#555', textAlign: 'center' },
+    itemId: { fontSize: 14, color: '#888', marginTop: 10 },
+    })
+
+export default DetailsScreen
